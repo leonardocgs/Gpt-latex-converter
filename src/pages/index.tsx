9218425313249c738c0eecf0022ef6a301e1166f
Leonardo Souza <leonardocesargoes@gmail.com>
2023-05-20 21:01:10 -0300

    style: add index page style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,44 +8,56 @@ export default function Home() {
   const [expressionToTranslate, setExpressionToTranslate] = useState("");
   const [translatedExpression, setTranslatedExpression] = useState("");
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <form
-        onSubmit={async (e) => {
-          e.preventDefault();
-          try {
-            const response = await fetch("/api/latex", {
-              method: "POST",
-              headers: {
-                contentType: "application/json",
-              },
-              body: JSON.stringify({ expression: expressionToTranslate }),
-            });
-            const { response: latex } = await response.json();
-            setTranslatedExpression(latex);
-          } catch (err) {
-            console.log(err);
-          }
-        }}
+    <>
+      <main
+        className={` min-h-screen text-center font-sans  p-24 ${inter.className}`}
       >
-        <label htmlFor="expressionToTranslate">
-          Digite a expressão que deseja traduzir para latex
-        </label>
-        <input
-          id="expressionToTranslate"
-          type="text"
-          onChange={(e) => setExpressionToTranslate(e.target.value)}
-        />
-        <button type="submit">Traduzir</button>
-      </form>
-      {translatedExpression && (
-        <div>
-          <p>{translatedExpression}</p>
+        <h1 className="text-4xl font-bold ">
+          Expression to <MathJax>{"\\(\\LaTeX\\)"}</MathJax> converter
+        </h1>
+        <div
+          className={`flex text-center min-h-screen gap-[20px] font-sans  p-24 ${inter.className}`}
+        >
+          <form
+            className="flex-1"
+            onSubmit={async (e) => {
+              e.preventDefault();
+              try {
+                const response = await fetch("/api/latex", {
+                  method: "POST",
+                  headers: {
+                    contentType: "application/json",
+                  },
+                  body: JSON.stringify({ expression: expressionToTranslate }),
+                });
+                const { response: latex } = await response.json();
+                setTranslatedExpression(latex);
+              } catch (err) {
+                console.log(err);
+              }
+            }}
+          >
+            <textarea
+              className="text-lg w-full min-h-[500px] p-4 border-2 border-gray-300 rounded-md resize-none"
+              id="expressionToTranslate"
+              placeholder="Expression to convert"
+              onChange={(e) => setExpressionToTranslate(e.target.value)}
+            />
+            <button type="submit" className="bg-blue-100 p-5 ">
+              Convert
+            </button>
+          </form>
+          {translatedExpression && (
+            <div className="flex-1 text-3xl">
+              <h2 className="font-bold ">Preview</h2>
 
-          <MathJax>{`\\(${translatedExpression}\\)`}</MathJax>
+              <MathJax>{`\\(${translatedExpression}\\)`}</MathJax>
+              <h2 className="font-bold mt-5">Latex Code</h2>
+              <p>{translatedExpression}</p>
+            </div>
+          )}
         </div>
-      )}
-    </main>
+      </main>
+    </>
   );
 }
